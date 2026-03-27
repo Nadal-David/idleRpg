8532a7411d049e47057e494af53cb075e24c17b2
David Nadal <nadal@Host-003.lan>
2026-03-27 11:45:07 +0100

    https

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as path from 'path';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(process.cwd(), 'certs/localhost-key.pem')),
+    cert: fs.readFileSync(path.join(process.cwd(), 'certs/localhost.pem')),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  app.enableCors({
+    origin: 'https://localhost:4200',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
